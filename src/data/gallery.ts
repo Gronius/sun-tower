d44d3fb3d870e2b9a924a0d6a1911fa3dbbb5d68
Volodymyr Vlasenko <vlivonika@gmail.com>
2026-08-01 10:49:35 +0200

    Create Gallery section

---
+++ src/data/gallery.ts
@@ -0,0 +1,31 @@
+export interface GalleryItem {
+  image: string;
+  alt: string;
+}
+
+export const gallery: GalleryItem[] = [
+  {
+    image: "/images/gallery/gallery-01.jpg",
+    alt: "Фасад житлового комплексу",
+  },
+  {
+    image: "/images/gallery/gallery-02.jpg",
+    alt: "Внутрішній двір",
+  },
+  {
+    image: "/images/gallery/gallery-03.jpg",
+    alt: "Дитячий майданчик",
+  },
+  {
+    image: "/images/gallery/gallery-04.jpg",
+    alt: "Лобі будинку",
+  },
+  {
+    image: "/images/gallery/gallery-05.jpg",
+    alt: "Зона відпочинку",
+  },
+  {
+    image: "/images/gallery/gallery-06.jpg",
+    alt: "Вечірній вигляд комплексу",
+  },
+];
